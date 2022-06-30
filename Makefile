eee2a822f08438d71f49dffadd74fe3d9b3c0c99
airgedon <104425308+airgedon@users.noreply.github.com>
2022-06-30 14:08:25 +0600

    Hello world

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
 CC = fasm
 .PHONY: default compile build once dump hex run
 default: build run
-compile: asmlib/fmt.asm asmlib/str.asm asmlib/sys.asm asmlib/mth.asm
-	fasm asmlib/fmt.asm && fasm asmlib/str.asm && fasm asmlib/sys.asm && fasm asmlib/mth.asm
 build: main.asm
 	$(CC) main.asm
 	ld main.o $(LIBS) -o main
--- main.asm
+++ main.asm
@@ -1,7 +1,16 @@
-format  ELF64 
-public _start           ;declare certain procedure, like an entry point
+format  ELF64		; specify the format of 64-bit system 
+public _start           ; declare certain procedure, like an entry point
 
+
+replace equ 0xA
+msg db "Hello World!", replace, 0    ; msg - variable db - data bytes, we should put 0 for strings
+len = $-msg
 _start:
+        mov rax, 4 ; 4 - write
+	mov rbx, 1 ; 1 - stdout
+	mov rcx, msg
+	mov rdx, len
+	int 0x80
 	call exit
 
 exit:
