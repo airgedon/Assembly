9bc701f420015a135d322dcbd298c6c1d8683fbe
airgedon <104425308+airgedon@users.noreply.github.com>
2022-06-30 14:11:00 +0600

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -6,16 +6,16 @@ replace equ 0xA
 msg db "Hello World!", replace, 0    ; msg - variable db - data bytes, we should put 0 for strings
 len = $-msg
 _start:
-        mov rax, 4 ; 4 - write
-	mov rbx, 1 ; 1 - stdout
+        mov rax, 4    ; 4 - write
+	mov rbx, 1    ; 1 - stdout
 	mov rcx, msg
 	mov rdx, len
 	int 0x80
 	call exit
 
 exit:
-	mov	rax, 1 ; 1 - exit
-	mov	rbx, 0 ; 0 - return 
+	mov	rax, 1    ; 1 - exit
+	mov	rbx, 0    ; 0 - return 
 	int 0x80
 
 
